ce6a6d70eaa142b00f058b12bb8e4c4985a1a6c1
Erik Pehrers <erik.pehrers@jetshop.se>
2020-04-26 22:19:26 +0200

    first commit

---
+++ packages/web-app/.next/static/development/pages/index.js
@@ -0,0 +1,153 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["static/development/pages/index.js"],{
+
+/***/ "../../node_modules/bar/index.js":
+/*!*************************************************************************************************!*\
+  !*** /Users/eripeh/Documents/local/projects/with-yarn-workspaces-app/node_modules/bar/index.js ***!
+  \*************************************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "../../node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _this = undefined,
+    _jsxFileName = "/Users/eripeh/Documents/local/projects/with-yarn-workspaces-app/node_modules/bar/index.js";
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+
+var Bar = function Bar() {
+  return __jsx("strong", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 3,
+      columnNumber: 19
+    }
+  }, "bar");
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Bar);
+
+/***/ }),
+
+/***/ "../../node_modules/foo/index.js":
+/*!*************************************************************************************************!*\
+  !*** /Users/eripeh/Documents/local/projects/with-yarn-workspaces-app/node_modules/foo/index.js ***!
+  \*************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = 'foo'
+
+
+/***/ }),
+
+/***/ "../../node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F&absolutePagePath=%2FUsers%2Feripeh%2FDocuments%2Flocal%2Fprojects%2Fwith-yarn-workspaces-app%2Fpackages%2Fweb-app%2Fpages%2Findex.js!./":
+/*!**********************************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** /Users/eripeh/Documents/local/projects/with-yarn-workspaces-app/node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F&absolutePagePath=%2FUsers%2Feripeh%2FDocuments%2Flocal%2Fprojects%2Fwith-yarn-workspaces-app%2Fpackages%2Fweb-app%2Fpages%2Findex.js ***!
+  \**********************************************************************************************************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+    (window.__NEXT_P=window.__NEXT_P||[]).push(["/", function() {
+      var mod = __webpack_require__(/*! ./pages/index.js */ "./pages/index.js")
+      if(true) {
+        module.hot.accept(/*! ./pages/index.js */ "./pages/index.js", function() {
+          if(!next.router.components["/"]) return
+          var updatedPage = __webpack_require__(/*! ./pages/index.js */ "./pages/index.js")
+          next.router.update("/", updatedPage)
+        })
+      }
+      return mod
+    }]);
+  
+
+/***/ }),
+
+/***/ "../../node_modules/react/index.js":
+/*!***********************************************************************************************!*\
+  !*** delegated ../../node_modules/react/index.js from dll-reference dll_2792696c4305f10043ec ***!
+  \***********************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = (__webpack_require__(/*! dll-reference dll_2792696c4305f10043ec */ "dll-reference dll_2792696c4305f10043ec"))("../../node_modules/react/index.js");
+
+/***/ }),
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "../../node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var foo__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! foo */ "../../node_modules/foo/index.js");
+/* harmony import */ var foo__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(foo__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var bar__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! bar */ "../../node_modules/bar/index.js");
+var _this = undefined,
+    _jsxFileName = "/Users/eripeh/Documents/local/projects/with-yarn-workspaces-app/packages/web-app/pages/index.js";
+
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+
+/* harmony default export */ __webpack_exports__["default"] = (function () {
+  return __jsx("div", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5,
+      columnNumber: 3
+    }
+  }, "Imported modules from another workspace:", __jsx("pre", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 7,
+      columnNumber: 5
+    }
+  }, foo__WEBPACK_IMPORTED_MODULE_1___default.a), __jsx(bar__WEBPACK_IMPORTED_MODULE_2__["default"], {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 8,
+      columnNumber: 5
+    }
+  }));
+});
+
+/***/ }),
+
+/***/ 1:
+/*!************************************************************************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2F&absolutePagePath=%2FUsers%2Feripeh%2FDocuments%2Flocal%2Fprojects%2Fwith-yarn-workspaces-app%2Fpackages%2Fweb-app%2Fpages%2Findex.js ***!
+  \************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2F&absolutePagePath=%2FUsers%2Feripeh%2FDocuments%2Flocal%2Fprojects%2Fwith-yarn-workspaces-app%2Fpackages%2Fweb-app%2Fpages%2Findex.js! */"../../node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F&absolutePagePath=%2FUsers%2Feripeh%2FDocuments%2Flocal%2Fprojects%2Fwith-yarn-workspaces-app%2Fpackages%2Fweb-app%2Fpages%2Findex.js!./");
+
+
+/***/ }),
+
+/***/ "dll-reference dll_2792696c4305f10043ec":
+/*!*******************************************!*\
+  !*** external "dll_2792696c4305f10043ec" ***!
+  \*******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = dll_2792696c4305f10043ec;
+
+/***/ })
+
+},[[1,"static/runtime/webpack.js"]]]);
+//# sourceMappingURL=index.js.map
